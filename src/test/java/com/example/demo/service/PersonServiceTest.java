5ad17571df89d8f0343d92bd2760c519b6561491
zabioz <zabioz00@gmail.com>
2020-03-03 01:34:44 +0900

    途中コミット2

--- src/test/java/com/example/demo/service/PersonServiceTest.java
+++ src/test/java/com/example/demo/service/PersonServiceTest.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
@@ -11,19 +11,33 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.dbunit.DbUnitTestExecutionListener;
-import org.springframework.test.dbunit.annotation.DatabaseSetup;
-import org.springframework.test.dbunit.annotation.ExpectedDatabase;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+//import com.github.springtestdbunit.annotation.ExpectedDatabase;
 import com.example.demo.entity.Person;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-		DbUnitTestExecutionListener.class })
-
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
 public class PersonServiceTest {
 
 	@Autowired
 	private PersonService personService;
+
+	@Test
+	@DatabaseSetup("sampleData.xml")
+	public void testFind() throws Exception {
+		List<Person> personList = this.personService.find("hil");
+		assertEquals(1, personList.size());
+		assertEquals("Phillip", personList.get(0).getFirstName());
+	}
+
+//	@Test
+//	@DatabaseSetup("sampleData.xml")
+//	@ExpectedDatabase("expectedData.xml")
+//	public void testRemove() throws Exception {
+//		this.personService.remove(1);
+//	}
+
 }
